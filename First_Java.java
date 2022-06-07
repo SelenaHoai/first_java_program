8404c1eec668dd286f010e089e28d0d83f2ede77
Selena Nguyen <missselenah@gmail.com>
2022-06-06 19:23:19 -0700

    First Java

---
+++ First_Java.java
@@ -0,0 +1,6 @@
+public class First_Java {
+    public static void main (String[] args) {
+        String newline = System.lineSeparator();
+        System.out.println("My name is Coding Dojo" + newline + "I am 100 years old" + newline + "My hometown is Burbank, CA");
+    }
+}
